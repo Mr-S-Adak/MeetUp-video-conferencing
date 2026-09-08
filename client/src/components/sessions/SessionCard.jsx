a4aab315fed895f7249202ecd607e8d7f72485de
Mr-S-Adak <sabujadak70@gmail.com>
2026-09-08 23:24:21 +0530

    Fix SessionCard component

--- client/src/components/sessions/SessionCard.jsx
+++ client/src/components/sessions/SessionCard.jsx
@@ -1,4 +1,4 @@
-import { CalendarIcon, UserIcon } from "lucide-react";
+import { CalendarIcon, MessageSquareIcon, UserIcon } from "lucide-react";
 import React from "react";
 
 const SessionCard = ({ session, onOpenDetails, onRejoin }) => {
@@ -41,6 +41,31 @@ const SessionCard = ({ session, onOpenDetails, onRejoin }) => {
             <strong>{session.participants?.length || 0}</strong> Participants
           </span>
         </div>
+
+        <div className="flex items-center gap-2 text-xs bg-slate-500/5 p-2.5 rounded-xl">
+          <MessageSquareIcon className="w-4 h-4 text-primary" />
+          <span>
+            <strong>{session.messages?.length || 0}</strong> Messages
+          </span>
+        </div>
+      </div>
+
+      {/* Action */}
+      <div className="flex items-center justify-between gap-3 pt-2">
+        <button
+          onClick={() => onOpenDetails(session.id)}
+          className="w-full bg-slate-400/20 text-slate-800 font-medium py-2.5 px-4 rounded-full text-xs transition-all cursor-pointer"
+        >
+          View Details
+        </button>
+        {!isEnded && (
+          <button
+            onClick={() => onRejoin(session.meetingId)}
+            className="w-full bg-primary hover:bg-primary-hover text-white font-medium py-2.5 px-4 rounded-full text-xs transition-all shadow-xs cursor-pointer text-center"
+          >
+            Re-Join
+          </button>
+        )}
       </div>
     </div>
   );
