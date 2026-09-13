82589a41cb5e6c51ad31d37a1ddcce3dfb4d8af9
Mr-S-Adak <sabujadak70@gmail.com>
2026-09-13 21:49:51 +0530

    Backend Server Completed

--- server/controllers/webhookController.js
+++ server/controllers/webhookController.js
@@ -19,7 +19,7 @@ export const handleClerkWebhook = async (req, res) => {
         INSERT INTO users (id,name,email,image,plan)
         VALUES (${userId},${name},${primaryEmail},${image},${plan})
         ON CONFLICT (id) DO UPDATE SET
-        email = EXCLUDED.email,
+        email = COALESCE(NULLIF(EXCLUDED.email, ''), user.email),
         id = EXCLUDED.id,
         name = EXCLUDED.name,
         image = EXCLUDED.image,
@@ -38,7 +38,7 @@ export const handleClerkWebhook = async (req, res) => {
         INSERT INTO users (id,name,email,image)
         VALUES (${userId},${name},${primaryEmail},${image},)
         ON CONFLICT (id) DO UPDATE SET
-          email = EXCLUDED.email,
+        email = EXCLUDED.email,
         id = EXCLUDED.id,
         name = EXCLUDED.name,
         image = EXCLUDED.image,
--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import http from "http";
 import cookieParser from "cookie-parser";
 import { initDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { handleClerkWebhook } from "./controllers/webhookController.js";
 import meetingRouter from "./routes/meetingsRoutes.js";
+import { Server } from "socket.io";
+import { setupSocketIo } from "./socket.js";
+import { error } from "console";
 
 const app = express();
+const server = http.createServer(app);
 
 // Connect to neon & Initialize tables
 await initDB();
@@ -28,8 +33,20 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => res.send("API is live!"));
 app.use("/api/meetings", meetingRouter);
 
+const io = new Server(server, {
+  cors: { origin: allowedOrigins, credentials: true },
+});
+
+setupSocketIo(io);
+
+// Centralized Error Handler
+app.use((err, _req, res, _next) => {
+  console.error(`[Error] ${err.message}`);
+  res.status(500).json({ error: "Internal server error" });
+});
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
