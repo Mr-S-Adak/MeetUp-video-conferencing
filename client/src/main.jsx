07c29eb01b6481382ac3e4a49be47c86ae79691d
Mr-S-Adak <sabujadak70@gmail.com>
2026-09-20 23:28:15 +0530

    main.jsx change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,14 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById("root")).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/login">
+  <ClerkProvider
+    publishableKey={PUBLISHABLE_KEY}
+    signInUrl="/login"
+    signUpUrl="/signup"
+    signInFallbackRedirectUrl="/"
+    signUpFallbackRedirectUrl="/"
+    afterSignOutUrl="/login"
+  >
     <BrowserRouter>
       <App />
     </BrowserRouter>
